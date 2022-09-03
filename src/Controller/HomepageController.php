0b1ce76ce1f5500b78a8a3ec4e863744060e522c
Patrycjuš August Sinkevič <patrycjus-august.sinkevic@stud.vilniustech.lt>
2022-09-03 11:33:23 +0300

    empty controllers TODO

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+}
